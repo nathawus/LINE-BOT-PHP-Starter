e64e4ad6c5ed0d88e54f52694be287c399944c24
nathawus <nathawuts@hotmail.com>
2017-08-11 22:33:51 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -41,7 +41,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $message;
+  $arrPostData['messages'][0]['text'] = $result;
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
