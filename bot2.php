8a556811f7a47a1947034f4f86c0200c36abca8d
nathawus <nathawuts@hotmail.com>
2017-08-11 22:42:16 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -28,8 +28,9 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 }else if(substr($arrJson['events'][0]['message']['text'],0,3) == "reg"){
   $ch = curl_init('http://www.d-mtonline.com/shop/line/register_line.php');
+  $param = '&id=
   curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, '&cid=999999999');
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $result = curl_exec($ch);
  
